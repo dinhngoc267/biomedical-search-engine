81c0b07d26f761f321d4e83bd060c9e3739cd2c3
Lê Đình Ngọc <dinhngoc123@gmail.com>
2021-06-04 16:17:19 +0700

    modify

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.7
 
+RUN pip install Flask gunicorn
+
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
@@ -7,5 +9,5 @@ RUN pip3 install -r requirements.txt
 
 COPY . .
 ENV PORT 8080
-EXPOSE 8080
-CMD [ "python3", "-m" , "flask", "run", "--host","0.0.0.0","-p", "8080"]
+CMD exec gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 0 app:app
+#CMD [ "python3", "-m" , "flask", "run", "--host","0.0.0.0","-p", "8080"]
--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, render_template, request, jsonify
 import biosyn.model as model
 import json
 
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+json_dump = json.dumps({'a': a, 'aa': [2, (2, 3, 4), a], 'bb': [2]}, cls=NumpyEncoder)
+print(json_dump)
+
 app = Flask(__name__, static_folder='static')
 
 @app.route("/", methods=['GET'])
-def home():
+def main():
+    print('here')
     return render_template("index.html")
 
 @app.route("/", methods=['POST'])
@@ -13,11 +23,13 @@ def search_query():
     data = json.loads(request.data)
     query = data.get('query')
     candidate_indices = model.model.retreival([query])
-    candidate_names = model.model.dictionary.data[candidate_indices][:,0]
+    candidate_names = model.model.dictionary.data[candidate_indices]
+    candidate_names = candidate_names.squeeze()[:,0]
+    #json_dump = json.dumps({'data': candidate_names}, cls=NumpyEncoder)
     return jsonify(result=1, data=candidate_names)
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-    app.run(debug=True, use_reloader=False) 
+    app.run(debug=True, use_reloader=False, host='0.0.0.0',  port=8080) 
 
